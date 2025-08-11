df0932e81fb9c82ce0b8dfd244508792eeec15bb
umut-burdur <umutburdurmetu@gmail.com>
2025-08-11 20:31:35 +0300

    Files arranged and Handling SQL with Flask Web Application added

---
+++ aws/projects/flask-01-02-hello-world-app-Jinja-Template/flask-02-Jinja_Template/jinja.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def head():
+    return render_template('index.html', number1 = 117000, number2 = 229000)
+
+@app.route('/sum')
+def number():
+    var1, var2 = 15210, 38960
+    return render_template('body.html', value1 = var1, value2 = var2, sum = var1 + var2)
+
+
+if __name__ == '__main__':
+    # app.run(debug=True) #add 'port=2000' in parant. to change ports
+    app.run(host= '0.0.0.0', port=80)
